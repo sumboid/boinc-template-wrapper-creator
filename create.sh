ea95e9e2cb2298296592078dcb1c908cff2d8eae
Ilya Sumbatyants <ilya.sumb@gmail.com>
2014-01-24 05:11:16 +0700

    extend

--- create.sh
+++ create.sh
@@ -1,7 +1,19 @@
 #!/usr/bin/env bash
 
-project=$1
-projectname=$2
+projectname=$1
+project=~/projects/$projectname
+projecthost=$2
+scriptpwd=`pwd`
+
+echo $scriptpwd
+
+cd ~/boinc-master/tools
+./make_project --delete_prev_inst --drop_db_first --url_base http://$projecthost --test_app $projectname
+cd ~/projects/$projectname
+su -c "cat ${projectname}.httpd.conf >> /etc/apache2/httpd.conf"
+su -c "apache2ctl -k restart"
+crontab ${projectname}.cronjob
+cd $scriptpwd
 
 rm -rf "$project/apps"
 rm -rf "$project/templates"
